3f3d45a4eef76c90444e41e46c1815362352f51f
Dallas Elliott <iop098321qwe@pm.me>
2023-09-18 17:22:16 -0600

    Update the update script

--- .update_commands.sh
+++ .update_commands.sh
@@ -1,8 +1,27 @@
 #!/bin/bash
 
-# Define the source and target directories
-SOURCE_DIR=~/Documents/github_repositories/custom_bash_commands
-TARGET_DIR=~
+# Temporary directory for sparse checkout
+SPARSE_DIR=$(mktemp -d)
 
-# Copy and rename the file
-cp $SOURCE_DIR/custom_bash_commands.sh $TARGET_DIR/.custom_bash_commands.sh
+# URL of the GitHub repository and the desired file path
+REPO_URL=https://github.com/iop098321qwe/custom_bash_commands.git
+FILE_PATH=custom_bash_commands.sh
+TARGET_FILE=~/.custom_bash_commands.sh
+
+# Initialize an empty git repository and configure for sparse checkout
+cd $SPARSE_DIR
+git init
+git remote add origin $REPO_URL
+git config core.sparseCheckout true
+echo $FILE_PATH >> .git/info/sparse-checkout
+
+# Fetch only the desired file
+git pull origin master
+
+# Copy the fetched file to the target location and overwrite if it exists
+cp $SPARSE_DIR/$FILE_PATH $TARGET_FILE
+
+# Clean up
+rm -rf $SPARSE_DIR
+cd ~
+clear
