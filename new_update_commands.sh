1bf5b00f26a02735933d79835ae057a758b8bfa2
Dallas Elliott <iop098321qwe@pm.me>
2023-12-12 15:58:03 -0700

    update

--- new_update_commands.sh
+++ new_update_commands.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
-# Copy scripts from the local repository to the home directory and make executable
+# Copy the custom_bash_commands.sh script to the home directory and make executable
 cp custom_bash_commands.sh ~/.custom_bash_commands.sh && chmod +x ~/.custom_bash_commands.sh
+echo "Copied custom_bash_commands.sh"
+
+# Copy the update_commands.sh script to the home directory and make executable
 cp update_commands.sh ~/.update_commands.sh && chmod +x ~/.update_commands.sh
+echo "Copied update_commands.sh"
 
 # Copy the .version file to the home directory
 cp .version ~/.version
+echo "Copied .version"
 
 # Print completion message
 echo "Custom bash commands updated successfully."
