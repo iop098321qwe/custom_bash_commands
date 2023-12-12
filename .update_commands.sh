a0bdf8e3f21255831f650795db31a29125595f97
Dallas Elliott <iop098321qwe@pm.me>
2023-12-12 11:10:26 -0700

    update

--- .update_commands.sh
+++ .update_commands.sh
@@ -21,6 +21,23 @@ git pull origin master -q
 # Copy the fetched file to the target location and overwrite if it exists
 cp $SPARSE_DIR/$FILE_PATH $TARGET_FILE
 
+# Also copy the .update_commands.sh file
+FILE_PATH=.update_commands.sh
+TARGET_FILE=~/.update_commands.sh
+
+# Initialize an empty git repository and configure for sparse checkout
+cd $SPARSE_DIR
+git init -q
+git remote add origin $REPO_URL
+git config core.sparseCheckout true
+echo $FILE_PATH >> .git/info/sparse-checkout
+
+# Fetch only the desired file
+git pull origin master -q
+
+# Copy the fetched file to the target location and overwrite if it exists
+cp $SPARSE_DIR/$FILE_PATH $TARGET_FILE
+
 # Clean up
 rm -rf $SPARSE_DIR
 cd ~
