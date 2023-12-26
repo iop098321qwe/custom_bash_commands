e67c5b224efa5f6b96c97e65d407efcb98a5c7cb
Dallas Elliott <iop098321qwe@pm.me>
2023-12-26 16:07:00 -0700

    1.11.2

--- update_commands.sh
+++ update_commands.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
+# Temporary directory for sparse checkout
+SPARSE_DIR=$(mktemp -d)
+
 # URL of the GitHub repository
 REPO_URL=https://github.com/iop098321qwe/custom_bash_commands.git
-
-###################################################################################################################################################################
-
-# Temporary directory for sparse checkout
-SPARSE_DIR1=$(mktemp -d)
 
 # List of file paths to download and move
 FILE_PATHS=(
@@ -41,31 +39,6 @@ for path in "${FILE_PATHS[@]}"; do
     fi
 done
 
-###################################################################################################################################################################
-# TEST BRANCH
-###################################################################################################################################################################
-
-# Temporary directory for sparse checkout
-SPARSE_DIR2=$(mktemp -d)
-
-# List of file paths to download and move
-FILE_PATHS=(
-    custom_bash_commands.sh
-    .version
-    update_commands.sh
-)
-
-# Initialize an empty git repository and configure for sparse checkout for test branch
-cd $SPARSE_DIR
-git init -q
-git remote add origin test $REPO_URL
-git config core.sparseCheckout true
-
-# Add each file path to the sparse checkout configuration
-for path in "${FILE_PATHS[@]}"; do
-    echo $path >> .git/info/sparse-checkout
-done
-
 # Fetch only the desired files from the test branch
 git pull origin test -q
 
@@ -75,8 +48,6 @@ for path in "${FILE_PATHS[@]}"; do
         cp $SPARSE_DIR/$path ~/$new_filename
         echo "Copied $path to $new_filename"
 done
-
-###################################################################################################################################################################
 
 # Clean up
 rm -rf $SPARSE_DIR
