9b2856e42e3f4a0d35150fd45bece3378fb442c4
Dallas Elliott <iop098321qwe@pm.me>
2024-05-06 16:46:21 -0600

    update

--- update_commands.sh
+++ update_commands.sh
@@ -11,7 +11,7 @@ FILE_PATHS=(
     custom_bash_commands.sh
     .version
     update_commands.sh
-    .cbcconfig/apt_packages.conf
+    /.cbcconfig/apt_packages.conf
 )
 
 # Initialize an empty git repository and configure for sparse checkout
