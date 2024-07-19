5e4ac82364efc30d3b9a44785fb69d9d4600d580
Dallas Elliott <iop098321qwe@pm.me>
2024-07-19 15:13:06 -0600

    1.22.1

--- custom_bash_commands.sh
+++ custom_bash_commands.sh
@@ -2202,9 +2202,11 @@ if command -v hstr &> /dev/null; then
     # ensure synchronization between bash memory and history file
     export PROMPT_COMMAND="history -a; history -n; ${PROMPT_COMMAND}"
     # if this is interactive shell, then bind hstr to Ctrl-r (for Vi mode check doc)
-    if [[ $- =~ .*i.* ]]; then bind '"\C-r": "\e^ihstr -- \n"'; fi
+    if [[ $- =~ .*i.* ]]; then bind '"\C-r": "\C-a hstr -- \C-j"'; fi
     # if this is interactive shell, then bind 'kill last command' to Ctrl-x k
     if [[ $- =~ .*i.* ]]; then bind '"\C-xk": "\C-a hstr -k \C-j"'; fi
+    # Bind Vim keys
+    bind '"\C-r": "\e^ihstr -- \n"'
 fi
 
 ###################################################################################################################################################################
