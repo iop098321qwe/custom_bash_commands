81b7817da052a969217cc14efb2c03bedb3df007
Dallas Elliott <iop098321qwe@pm.me>
2024-12-03 19:11:06 -0700

    fix(alias): make `verg` use printf instead of echo

--- custom_bash_commands.sh
+++ custom_bash_commands.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-VERSION="2.9.6"
+VERSION="2.9.8"
 
 ###################################################################################################################################################################
 # CUSTOM BASH COMMANDS
@@ -2567,7 +2567,7 @@ alias test='source ~/Documents/github_repositories/custom_bash_commands/custom_b
 alias ucbc='updatecbc'
 alias vault='cd ~/Documents/grymms_grimoires && ls'
 alias ver='npx commit-and-tag-version'
-alias verg='ver && gpfom && echo "\n Run gh cr to create a release"'
+alias verg='ver && gpfom && printf "\n Run gh cr to create a release\n"'
 alias vim='nvim'
 alias v='nvim'
 alias x='chmod +x'
