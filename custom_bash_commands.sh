c2b70dfb3d754bf6f0e565a055bf520bd6e29f8e
iop098321qwe <iop098321qwe@pm.me>
2026-02-17 18:38:15 -0700

    chore(versioning): bump version number

--- custom_bash_commands.sh
+++ custom_bash_commands.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-CBC_VERSION="v306.20.0"
+CBC_VERSION="v306.21.0"
 
 ################################################################################
 # CUSTOM BASH COMMANDS (by iop098321qwe)
